bd9614c999eef053b0158bc784e9af8146bc4ea4
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-01-08 11:32:44 +0100

    Update 1 General NFT Trends.py

--- pages/1 General NFT Trends.py
+++ pages/1 General NFT Trends.py
@@ -86,7 +86,7 @@ SELECT
   avg(avg_nft_price) over (order by date) as cum_avg_price,
   count(purchaser) as users,
   sum(users) over (order by date) as cum_users
-from solana.core.fact_nft_sales where date>=current_date-INTERVAL '1 MONTH'
+from solana.core.fact_nft_sales where date>=current_date-INTERVAL '2 MONTHS'
 group by 1
 order by 1 asc
 """
